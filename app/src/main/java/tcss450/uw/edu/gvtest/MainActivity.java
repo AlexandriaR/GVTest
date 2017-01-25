a6a7cd4899b5a9b01bef3bfbfa7c010827e164d9
AlexandriaR <alexar22@uw.edu>
2017-01-24 16:22:00 -0800

    init commit

---
+++ app/src/main/java/tcss450/uw/edu/gvtest/MainActivity.java
@@ -0,0 +1,21 @@
+package tcss450.uw.edu.gvtest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+
+
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
