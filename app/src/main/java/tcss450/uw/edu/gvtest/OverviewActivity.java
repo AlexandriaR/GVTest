12b2e8b9a4cb40214ddf6ee6b72425ce3e6d77f9
Alexandria <alexar22@uw.edu>
2017-02-04 21:16:00 -0800

    Changes to UI. Added to styles.xml.  Custom button (with states).

--- app/src/main/java/tcss450/uw/edu/gvtest/OverviewActivity.java
+++ app/src/main/java/tcss450/uw/edu/gvtest/OverviewActivity.java
@@ -3,14 +3,23 @@ package tcss450.uw.edu.gvtest;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 public class OverviewActivity extends AppCompatActivity implements View.OnLongClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setProgressLabel();                             //not sure if this should be run here, or below setContentView
         setContentView(R.layout.activity_overview);
     }
+
+    private void setProgressLabel() {
+        ProgressBar p = (ProgressBar) this.findViewById(R.id.progressBar3);
+        TextView t = (TextView) this.findViewById(R.id.progressLabel);
+        t.setText(p.getProgress() + "\\" + p.getMax());
+    } //This *should* make the TextView over the progress bar be "current progress/maximum value"
 
     @Override
     public boolean onLongClick(View v) { //This is the start of trying to make a fragment show up when an item in the overview is tapped and held
