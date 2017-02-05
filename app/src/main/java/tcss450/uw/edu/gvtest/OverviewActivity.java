9ef93f036a8a050ab39a965d07f4bb88c9d802a4
Alexandria <alexar22@uw.edu>
2017-02-04 17:22:13 -0800

    Changes to UI stuff. Primarily textboxes.

--- app/src/main/java/tcss450/uw/edu/gvtest/OverviewActivity.java
+++ app/src/main/java/tcss450/uw/edu/gvtest/OverviewActivity.java
@@ -2,12 +2,18 @@ package tcss450.uw.edu.gvtest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
-public class OverviewActivity extends AppCompatActivity {
+public class OverviewActivity extends AppCompatActivity implements View.OnLongClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_overview);
+    }
+
+    @Override
+    public boolean onLongClick(View v) { //This is the start of trying to make a fragment show up when an item in the overview is tapped and held
+        return false;
     }
 }
